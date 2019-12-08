8b873f269f18d6cac16e300117ef5cf0ecfc663c
André Martini Diniz <58450200+Andredin@users.noreply.github.com>
2019-12-08 03:34:09 -0300

    Front Home

--- src/client/App.js
+++ src/client/App.js
@@ -2,32 +2,31 @@ import './app.css';
 
 import React, { Component } from 'react';
 import {
-  Route,
-  BrowserRouter as Router,
-  Switch
-} from "react-router-dom";
+    Route,
+    BrowserRouter as Router,
+    Switch
+} from 'react-router-dom';
+
+import { createMuiTheme } from '@material-ui/core/styles';
 
 import { Home } from './Home';
 import { Map } from './Map';
 
 export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/test">
-              <Home />
-            </Route>
-            <Route path="/dm/:dmId">
-              <Map />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div style={{ height: '100%' }}>
+                    <Switch>
+                        <Route exact path='/'>
+                            <Home />
+                        </Route>
+                        <Route path='/dm/:dmId'>
+                            <Map />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
 }
--- src/client/Home.jsx
+++ src/client/Home.jsx
@@ -1,29 +1,42 @@
 import React, { useEffect, useState } from 'react';
 
-import ReactImage from './react.png';
-import { useHistory } from "react-router-dom";
+import Button from '@material-ui/core/Button';
+import { useHistory } from 'react-router-dom';
+import { Grid, TextField, AppBar, Toolbar, Typography } from '@material-ui/core';
+import MapIcon from '@material-ui/icons/Map';
 
 export const Home = () => {
-    const [username, setUsername] = useState(null);
     const history = useHistory();
-
-    useEffect(() => {
-        fetch('/api/getUsername')
-        .then(res => res.json())
-        .then(user => setUsername(user.username));
-    });
+    const [url, setURL] = useState(0);
 
     const create = async () => {
-        const res = await fetch('/api/create', { method: 'POST'});
+        const data = { url : url }
+        const res = await fetch('/api/create', { method: 'POST', body: JSON.stringify(data)});
         const mapData = await res.json();
         history.push(`/dm/${mapData.idDm}`);
     }
 
     return (
-        <div>
-            {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-            <img src={ReactImage} alt="react" />
-            <button onClick={create}>create</button>
-        </div>
+        <Grid container style={{height: '100%'}}>
+            <Grid item xs={12} style={{height: '5%'}}>
+                <AppBar>
+                    <Toolbar>
+                        <Typography variant="h6">Dungeon Goggles</Typography>
+                    </Toolbar>
+                </AppBar>
+            </Grid>
+            <Grid item xs={12} style={{height: '95%'}}>
+                <Grid container spacing={2} justify={'center'} alignItems={'center'} style={{height: '100%'}}>
+                    <Grid item>
+                        <TextField style={{width: 500}} label='Image URL' type='search' variant='outlined' onChange={(event) => {setURL(event.target.value)}}/>
+                    </Grid>
+                    <Grid item>
+                        <Button size='large' variant='contained' onClick={create} startIcon={<MapIcon />}>
+                            Create
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
     );
 }
